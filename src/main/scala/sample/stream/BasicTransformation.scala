915bd4a1a4c7a9c34402f30796f419e9edd5b861
Mike Slinn <mslinn@micronauticsresearch.com>
2017-03-10 16:55:38 -0800

    Fussing

--- src/main/scala/sample/stream/BasicTransformation.scala
+++ src/main/scala/sample/stream/BasicTransformation.scala
@@ -5,7 +5,6 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 
 object BasicTransformation {
-
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem("Sys")
     import system.dispatcher
@@ -13,10 +12,11 @@ object BasicTransformation {
     implicit val materializer = ActorMaterializer()
 
     val text =
-      """|Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      """Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         |Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         |when an unknown printer took a galley of type and scrambled it to make a type
-        |specimen book.""".stripMargin
+        |specimen book.
+        |""".stripMargin
 
     Source.fromIterator(() => text.split("\\s").iterator).
       map(_.toUpperCase).
--- src/main/scala/sample/stream/GroupLogFile.scala
+++ src/main/scala/sample/stream/GroupLogFile.scala
@@ -1,53 +1,44 @@
 package sample.stream
 
 import java.nio.file.Paths
-
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
-
-import scala.util.Failure
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 object GroupLogFile {
-
   def main(args: Array[String]): Unit = {
-    // actor system and implicit materializer
     implicit val system = ActorSystem("Sys")
     implicit val materializer = ActorMaterializer()
 
-    // execution context
-
     val LoglevelPattern = """.*\[(DEBUG|INFO|WARN|ERROR)\].*""".r
-
-    // read lines from a log file
     val logFile = Paths.get("src/main/resources/logfile.txt")
 
-    FileIO.fromPath(logFile).
-      // parse chunks of bytes into lines
-      via(Framing.delimiter(ByteString(System.lineSeparator), maximumFrameLength = 512, allowTruncation = true)).
-      map(_.utf8String).
-      map {
+    FileIO.fromPath(logFile) // parse chunks of bytes into lines:
+      .via(Framing.delimiter(ByteString(System.lineSeparator), maximumFrameLength = 512, allowTruncation = true))
+      .map(_.utf8String)
+      .map {
         case line @ LoglevelPattern(level) => (level, line)
         case line @ other                  => ("OTHER", line)
-      }.
-      // group them by log level
-      groupBy(5, _._1).
-      fold(("", List.empty[String])) {
+      }
+      .groupBy(5, _._1) // group by log level
+      .fold(("", List.empty[String])) {
         case ((_, list), (level, line)) => (level, line :: list)
-      }.
-      // write lines of each group to a separate file
-      mapAsync(parallelism = 5) {
+      }
+      .mapAsync(parallelism = 5) { // write lines of each group to a separate file
         case (level, groupList) =>
-          Source(groupList.reverse).map(line => ByteString(line + "\n")).runWith(FileIO.toPath(Paths.get(s"target/log-$level.txt")))
-      }.
-      mergeSubstreams.
-      runWith(Sink.onComplete {
+          Source(groupList.reverse)
+            .map(line => ByteString(line + "\n"))
+            .runWith(FileIO.toPath(Paths.get(s"target/log-$level.txt")))
+      }
+      .mergeSubstreams
+      .runWith(Sink.onComplete {
         case Success(_) =>
           system.terminate()
+
         case Failure(e) =>
-          println(s"Failure: ${e.getMessage}")
+          println(s"Failure: ${ e.getMessage }")
           system.terminate()
       })
   }
